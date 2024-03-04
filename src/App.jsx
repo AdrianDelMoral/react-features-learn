e2a80a28111f089d5b4c0d5d32ba27424c20ecce
Adrián Del Moral Martín <iFullStart@gmail.com>
2024-03-04 15:32:22 +0100

    Using the hook useState to be able to have dynamically the information of our TabButton component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
 // Siguiente video: https://youtu.be/cifTVzfIwrY?list=PLO8lO9oepSLv0zh2j644zWefsxEGR8zdC&t=2
 
 import Header from './components/Header/Header.jsx';
-
 //* Constante CoreConcepts para la información que se añade en el componente CoreConcepts
 import { CORE_CONCEPTS } from './data.js'; 
-
 //* Componente para poder poner
 import CoreConcepts from './components/CoreConcepts/CoreConcepts.jsx';
 import TabButton from './components/TabButton/TabButton.jsx';
+import { useState } from 'react';
 
 function App() {
-  //* Evento que registra que pulso un botón
-  /** 
-    *? En JavaScript:
-    *? document.querySelector("button").addEventListener("click", ()=>{
-    *?  console.log('Ey! Estás haciendo click en los botones!')
-    *? })
+  //* Eventos que registran que se pulsa un botón
+  /* 
+    ? En JavaScript:
+    ? document.querySelector("button").addEventListener("click", ()=>{
+    ?  console.log('Ey! Estás haciendo click en los botones!')
+    ? })
   */
 
-    
   //* handleClick = manejador de clicks
   //* Evento en React:
+  /* 
+    * Se usa en la parte superior del componente, justo debajo de la declaración del componente App.jsx
+    * Reglas que deben seguir las funciones reactHooks: 
+    * 1. dentro de funciones componentes, o hooks creados por nosotros, no se puede llamar dentro de otras funciones, o dentro de ifs/bucles/etc tampoco
+    * 2. Solo pueden ser llamadas en la parte más superior de los componentes o hooks customizables. Lo primero que debe tener la función
+  */
+  //? valor que podemos guardar en una variable o constante, 
+  //? y devolverá un array con dos valores: "nombreVariable", "setnombreVariable"
+  //? set = establecer
+  //? nombreVariable = contendrá la información más actual
+  //? setNombreVariable = contendrá la función para actualizar ese nombreVariable
+
+  const [selectedTopic, setSelectedTopic] = useState("Por favor, pulsa un botón del menú"); 
+
   function handleClickMenu(selectedButton){
-    console.log(`Ey! Estás pulsando ${selectedButton} botón de mi menú!`);
+    setSelectedTopic(selectedButton);
+    console.log(`Ey! Estás pulsando el botón ${selectedTopic}`);
   }
 
   return (
@@ -41,17 +54,10 @@ function App() {
           <CoreConcepts {...CORE_CONCEPTS[3]}/>
           
           {/* Antes de poner el spread operator "...":
-            <CoreConcepts 
-              imagePath={CORE_CONCEPTS[3].imagePath}  
-              title={CORE_CONCEPTS[3].title} 
-              description={CORE_CONCEPTS[3].description}/>
-          */}
+            <CoreConcepts imagePath={CORE_CONCEPTS[3].imagePath}  title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description}/> */}
           
           {/* Pasando las variables a mano:
-          <CoreConcepts 
-            imagePath={componentsImage} 
-            title="Componentes" 
-            description="El úcleo principal de construcción de una UI con React. La combinación de distintos componentes logra una composición de una UI más manejable"/> */}
+          <CoreConcepts imagePath={componentsImage} title="Componentes" description="El núcleo principal de construcción de una UI con React. La combinación de distintos componentes logra una composición de una UI más manejable"/> */}
 
         </div>
       </section>
@@ -59,21 +65,22 @@ function App() {
       <section id='examples'>
         <h2>Ejemplo React</h2>
         <menu className='tab'>
-          {/* Se ejecutará handleClickMenu cuando se pulse, 
-              no cuando el navegador lea la linea de código
-              si tuviese parentesis handleClickMenu() se llamaria nada más leer la linea
+          {/* Se ejecutará handleClickMenu cuando se pulse, no cuando el navegador lea la 
+              linea de código si tuviese parentesis handleClickMenu() se llamaria nada más 
+              leer la linea
 
-              en caso de tener arrowFunction ()=> nombreFuncion() no pasa nada 
-              si ponemos el parentesis, ya que recibe la declaración, pero no 
-              la ejecuta si no solo la arrow function
+              en caso de tener arrowFunction ()=> nombreFuncion() no pasa nada si ponemos el 
+              parentesis, ya que recibe la declaración, pero no la ejecuta si no solo la 
+              arrow function
           */}
-          {/* Custom Parameters in the Dinamic Events */}
           <TabButton onClick={()=>handleClickMenu("Componentes")}>Componentes</TabButton>
           <TabButton onClick={()=>handleClickMenu("JSX")}>JSX</TabButton>
           <TabButton onClick={()=>handleClickMenu("Props")}>Props</TabButton>
           <TabButton onClick={()=>handleClickMenu("Estados")}>Estados</TabButton>
         </menu>
-        Contenido Dinamico
+        
+        { selectedTopic }
+
       </section>
 
       <main>
--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -4,10 +4,6 @@ export default function TabButton({ children, onClick }) {
 
   return (
     <li>
-      {/* Se ejecutará handleClick cuando se pulse, 
-          no cuando el navegador lea la linea de código
-          si tuviese parentesis handleClick() se llamaria nada más leer la linea
-      */}
       <button onClick={onClick}>{ children }</button>
     </li>
   )
